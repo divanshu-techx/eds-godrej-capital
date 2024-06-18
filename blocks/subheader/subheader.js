98046990fc9eb5e7364919701cbf3552adabac5e
Ankit <ankit.saini@TechxSolution.com>
2024-06-18 15:09:56 +0530

    subHeader lint fixes

--- blocks/subheader/subheader.js
+++ blocks/subheader/subheader.js
@@ -4,31 +4,23 @@ export default async function decorate(block) {
   if (block.classList.contains('decorated')) {
     return;
   }
-  
   block.classList.add('decorated');
-
   const subHeaderMeta = getMetadata('subheader');
   const subHeaderPath = subHeaderMeta ? new URL(subHeaderMeta, window.location).pathname : '/subheader';
-
   try {
     const subHeaderFragment = await loadFragment(subHeaderPath);
-    
     if (subHeaderFragment && subHeaderFragment.firstElementChild) {
       const subHeader = document.createElement('div');
       subHeader.classList.add('subheader-wrapper');
-      
       while (subHeaderFragment.firstElementChild) {
         subHeader.append(subHeaderFragment.firstElementChild);
       }
-      
       const liElements = subHeader.querySelectorAll('li');
       liElements.forEach((li) => {
         li.classList.add('subheader-list');
       });
-      
       block.textContent = '';  // Clear the block's existing content
       block.append(subHeader); // Append the new subheader
-      
     } else {
       console.error('Subheader fragment is empty or malformed.');
     }
@@ -38,25 +30,25 @@ export default async function decorate(block) {
 }
 
 export async function loadFragment(path) {
-    try {
-      const response = await fetch(`${path}.plain.html`);
-      if (response.ok) {
-        const html = await response.text();
-        const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = html;
-        const subheader = tempDiv.querySelector('.subheader');
-        if (subheader) {
-          return subheader;
-        } else {
-          console.error(`No subheader found in the fragment from ${path}`);
-        }
+  try {
+    const response = await fetch(`${path}.plain.html`);
+    if (response.ok) {
+      const html = await response.text();
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = html;
+      const subheader = tempDiv.querySelector('.subheader');
+      if (subheader) {
+        return subheader;
       } else {
-        console.error(`Failed to load fragment from ${path}: ${response.statusText}`);
+        console.error(`No subheader found in the fragment from ${path}`);
       }
-    } catch (error) {
-      console.error('Error loading fragment:', error);
+    } else {
+      console.error(`Failed to load fragment from ${path}: ${response.statusText}`);
     }
-    return null;
+  } catch (error) {
+    console.error('Error loading fragment:', error);
   }
-  
-  
+  return null;
+}
+
+
