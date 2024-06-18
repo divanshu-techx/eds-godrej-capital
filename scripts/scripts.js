9a96b0b89a52cfa117ec1e69707202b334d41244
ImjeekxGurjar <jitender.rawat@techx-consulting.com>
2024-06-18 16:09:03 +0530

    sonar issue - v4

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,33 +11,34 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
-  decorateBlock,
-  loadBlock,
-} from "./lib-franklin.js";
-
-import loadContentFromURL from "../blocks/fragmentcards/fragmentcards.js";
-import loadBoardContentFromURL from "../blocks/boardmembers/boardmembers.js";
+} from './aem.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
-window.hlx.RUM_GENERATION = "project-1"; // add your RUM generation information here
-loadContentFromURL();
-loadBoardContentFromURL();
+
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector("h1");
-  const picture = main.querySelector("picture");
+  const h1 = main.querySelector('h1');
+  const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
-  if (
-    h1 &&
-    picture &&
-    h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING
-  ) {
-    const section = document.createElement("div");
-    section.append(buildBlock("hero", { elems: [picture, h1] }));
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
+  }
+}
+
+/**
+ * load fonts.css and set a session storage flag
+ */
+async function loadFonts() {
+  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+  try {
+    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
+  } catch (e) {
+    // do nothing
   }
 }
 
@@ -50,7 +51,7 @@ function buildAutoBlocks(main) {
     buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error("Auto Blocking failed", error);
+    console.error('Auto Blocking failed', error);
   }
 }
 
@@ -73,117 +74,23 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  document.documentElement.lang = "en";
+  document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
-  const main = doc.querySelector("main");
+  const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
-    aggregateTabSectionsIntoComponents(main);
-    document.body.classList.add("appear");
+    document.body.classList.add('appear');
     await waitForLCP(LCP_BLOCKS);
   }
-}
 
-/**
- * Adds the favicon.
- * @param {string} href The favicon URL
- */
-export function addFavIcon(href) {
-  const link = document.createElement("link");
-  link.rel = "icon";
-  link.type = "image/svg+xml";
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName("head")[0].appendChild(link);
-  }
-}
-const tabElementMap = {};
-
-function calculateTabSectionCoordinate(
-  main,
-  lastTabBeginningIndex,
-  targetTabSourceSection
-) {
-  if (!tabElementMap[lastTabBeginningIndex]) {
-    tabElementMap[lastTabBeginningIndex] = [];
-  }
-  tabElementMap[lastTabBeginningIndex].push(targetTabSourceSection);
-}
-
-function calculateTabSectionCoordinates(main) {
-  let lastTabIndex = -1;
-  let foldedTabsCounter = 0;
-  const mainSections = [...main.childNodes];
-  main.querySelectorAll("div.section[data-tab-title]").forEach((section) => {
-    const currentSectionIndex = mainSections.indexOf(section);
-
-    if (
-      lastTabIndex < 0 ||
-      currentSectionIndex - foldedTabsCounter !== lastTabIndex
-    ) {
-      // we construct a new tabs component, at the currentSectionIndex
-      lastTabIndex = currentSectionIndex;
-      foldedTabsCounter = 0;
+  try {
+    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
+      loadFonts();
     }
-
-    foldedTabsCounter += 2;
-    calculateTabSectionCoordinate(main, lastTabIndex, section);
-  });
-}
-
-async function autoBlockTabComponent(main, targetIndex, tabSections) {
-  // the display none will prevent a major CLS penalty.
-  // franklin will remove this once the blocks are loaded.
-  const section = document.createElement("div");
-  section.setAttribute("class", "section");
-  section.setAttribute("style", "display:none");
-  section.dataset.sectionStatus = "loading";
-  const tabsBlock = document.createElement("div");
-  tabsBlock.setAttribute("class", "tabs");
-
-  const tabContentsWrapper = document.createElement("div");
-  tabContentsWrapper.setAttribute("class", "contents-wrapper");
-
-  tabsBlock.appendChild(tabContentsWrapper);
-
-  tabSections.forEach((tabSection) => {
-    tabSection.classList.remove("section");
-    tabSection.classList.add("contents");
-    // remove display: none
-    tabContentsWrapper.appendChild(tabSection);
-    tabSection.style.display = null;
-  });
-  main.insertBefore(section, main.childNodes[targetIndex]);
-  section.append(tabsBlock);
-  decorateBlock(tabsBlock);
-  await loadBlock(tabsBlock);
-
-  // unset display none manually.
-  // somehow in some race conditions it won't be picked up by lib-franklin.
-  // CLS is not affected
-  section.style.display = null;
-}
-
-function aggregateTabSectionsIntoComponents(main) {
-  calculateTabSectionCoordinates(main);
-
-  // when we aggregate tab sections into a tab autoblock, the index get's lower.
-  // say we have 3 tabs starting at index 10, 12 and 14. and then 3 tabs at 18, 20 and 22.
-  // when we fold the first 3 into 1, those will start at index 10. But the other 3 should now
-  // start at 6 instead of 18 because 'removed' 2 sections.
-  let sectionIndexDelta = 0;
-  Object.keys(tabElementMap).map(async (tabComponentIndex) => {
-    const tabSections = tabElementMap[tabComponentIndex];
-    await autoBlockTabComponent(
-      main,
-      tabComponentIndex - sectionIndexDelta,
-      tabSections
-    );
-    sectionIndexDelta = tabSections.length - 1;
-  });
+  } catch (e) {
+    // do nothing
+  }
 }
 
 /**
@@ -191,21 +98,22 @@ function aggregateTabSectionsIntoComponents(main) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector("main");
+  const main = doc.querySelector('main');
   await loadBlocks(main);
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector("header"));
-  loadFooter(doc.querySelector("footer"));
+  loadHeader(doc.querySelector('header'));
+  loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
-  sampleRUM("lazy");
-  sampleRUM.observe(main.querySelectorAll("div[data-block-name]"));
-  sampleRUM.observe(main.querySelectorAll("picture > img"));
+  loadFonts();
+
+  sampleRUM('lazy');
+  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
+  sampleRUM.observe(main.querySelectorAll('picture > img'));
 }
 
 /**
@@ -214,7 +122,7 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import("./delayed.js"), 3000);
+  window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
 }
 
