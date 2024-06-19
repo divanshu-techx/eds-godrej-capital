1fb6831f9ae4e65eb24cd2e3ee90d65b577b8775
Sahil Kumar <sahilkumaryogesh@gmail.com>
2024-06-19 17:53:53 +0530

    sonar-issue-fixing

--- blocks/subheader/subheader.js
+++ blocks/subheader/subheader.js
@@ -3,6 +3,7 @@ import { loadFragment } from '../fragment/fragment.js';
 
 let isDecorated = false;
 
+
 export default async function decorate(block) {
   if (isDecorated) return;
   isDecorated = true;
