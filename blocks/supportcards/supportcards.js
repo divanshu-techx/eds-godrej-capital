3d57754b2ad9c6b6ed9017a15b8e3df2478f261e
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-02 19:26:47 +0530

    support landing page

--- blocks/supportcards/supportcards.js
+++ blocks/supportcards/supportcards.js
@@ -1,19 +1,106 @@
 export default async function decorate(block) {
-console.log('js called');
-console.log(block);
+// console.log('js called');
+// console.log(block);
 
 getIndexedChildNames('support-landing-cards');
 
- }
+
+  let element = document.querySelectorAll('.support-landing-cards-child')[1];
+  // console.log(element);
+  let pictureElement = element.querySelector('picture');
+  let img = pictureElement.querySelector('img').src;
+  let ImgEle=  element.querySelectorAll('div')[1];
+  ImgEle.style.backgroundImage = `url(${img})`;
+  ImgEle.style.width = '317px';
+  ImgEle.style.height = '281px';
+  ImgEle.style.backgroundSize = 'cover';
+  ImgEle.style.marginLeft = '-25px';
+  ImgEle.style.marginTop = '-35px';
+  ImgEle.style.borderRadius = '10px';
+  ImgEle.style.backgroundRepeat = 'no-repeat';
+  ImgEle.style.backgroundPosition = 'center center';
+  ImgEle.parentElement.style.overflow = 'hidden';
+  element.removeChild(element.firstElementChild);
+
+  let zerothDiv = document.querySelectorAll('.support-landing-cards-child')[0];
+  let firstDiv = document.querySelectorAll('.support-landing-cards-child')[1];
+  let secondDiv = document.querySelectorAll('.support-landing-cards-child')[2];
+  let thirdDiv = document.querySelectorAll('.support-landing-cards-child')[3];
+  let fourthDiv = document.querySelectorAll('.support-landing-cards-child')[4];
+  let fifthDiv = document.querySelectorAll('.support-landing-cards-child')[5];
+  let sixthDiv = document.querySelectorAll('.support-landing-cards-child')[6];
+  let seventhDiv = document.querySelectorAll('.support-landing-cards-child')[7];
+  let eigthDiv = document.querySelectorAll('.support-landing-cards-child')[8];
+const containerDivForCustomer = createDivElement('containerDivForCustomer');
+
+// image div
+const imageDiv = createDivElement('imageDiv');
+
+// get starting first div
+let forCustomerDivEle = zerothDiv;
+let imageDivEle = firstDiv;
+imageDiv.appendChild(forCustomerDivEle);
+imageDiv.appendChild(imageDivEle);
+containerDivForCustomer.appendChild(imageDiv);
+
+let parentEle= document.getElementsByClassName('support-landing-cards')[0];
 
 
-// Helper function to get index with appropriate suffix
-function getIndex(n) {
-  const s = ['th', 'st', 'nd', 'rd'];
-  const v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+let whatupDivEle = fourthDiv;
+containerDivForCustomer.appendChild(whatupDivEle);
+
+// get second div
+const containerDivForInvestor = createDivElement('containerDivForInvestor');
+
+const faqDiv = createDivElement('faqDiv');
+
+ // get starting first div
+let forInvestQuery = secondDiv;
+let forFaqElem =thirdDiv;
+faqDiv.appendChild(forInvestQuery);
+faqDiv.appendChild(forFaqElem);
+containerDivForInvestor.appendChild(faqDiv);
+
+
+
+// let parentEle= document.getElementsByClassName('support-landing-cards')[0];
+// parentEle.appendChild(containerDivForInvestor);
+// console.log(sixthDiv.querySelectorAll('div')[0]);
+// let finalDiv= document.createElement('div');
+let newFifth=fifthDiv.querySelectorAll('div')[0];
+let newSixth= sixthDiv.querySelectorAll('div')[0];
+let newDiv=document.createElement('div');
+newDiv.appendChild(newFifth);
+newDiv.appendChild(newSixth);
+newDiv.classList.add("newDiv");
+fifthDiv.appendChild(newDiv);
+
+let newSeventh=seventhDiv.querySelectorAll('div')[0];
+let newEigth= eigthDiv.querySelectorAll('div')[0];
+let customDiv=document.createElement('div');
+customDiv.classList.add("customDiv")
+customDiv.appendChild(newSeventh);
+customDiv.appendChild(newEigth);
+fifthDiv.appendChild(customDiv);
+fifthDiv.classList.add("fifthElement");
+sixthDiv.remove('support-landing-cards-child');
+seventhDiv.remove('support-landing-cards-child');
+eigthDiv.remove('support-landing-cards-child');
+
+
+
+containerDivForInvestor.append(fifthDiv);
+
+parentEle.appendChild(containerDivForCustomer);
+parentEle.appendChild(containerDivForInvestor);
+// custom code
+let data= document.querySelectorAll('.containerDivForInvestor')[0].lastElementChild;
+
+
+
+
 }
- 
+
 // Function to get indexed child names and set class with numbering
 function getIndexedChildNames(parentId) {
   const newParentId = `.${parentId}`;
@@ -26,8 +113,14 @@ function getIndexedChildNames(parentId) {
   const childDivs = parentElement.querySelectorAll(`${newParentId} > div`);
  
   childDivs.forEach((div, index) => {
-    const lastIndexed = getIndex(index + 1);
-    div.className = `${parentId}-child-${lastIndexed}`;
+    div.className = `${parentId}-child`;
   });
 }
  
+function createDivElement (className) {
+  let divEle = document.createElement('div');
+  divEle.className = className;
+  return divEle;
+}
+ 
+
