21f54bb0102520c6f3b86dd40faa15f599ba698f
Mohit Singh <mohit.singh@techx-consulting.com>
2024-07-02 11:48:11 +0530

    refactoring the blog listing component

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -8,25 +8,40 @@ const searchPlaceholderText = getDataAttributeValueByName('search-placeholder-te
 const filterLabel = getDataAttributeValueByName('filter-label');
 const productLabel = getDataAttributeValueByName('default-dropdown-label');
 const sortingFilters = getDataAttributeValueByName('sorting-filters');
+
+// Global variables
 let articlesPerPage = 2;
 let currentPage = 1;
-var responseData, sortedResData;
 
 // Main function to decorate the block
 export default async function decorate(block) {
+	// Render search section and initial elements
 	renderSearchSection(block);
-	responseData = await fetchData(queryIndexApiUrl);
-	console.log(responseData);
-	if (responseData != null) {
-		sortedResData =  [...responseData];
-		const categories = getDistinctCategories(responseData);
-		renderFiltersInDropdown();
-		renderCategoriesInDropdown(categories);
-		renderCards(block, responseData);
-		renderPagination(block, responseData);
+	createNoResultDiv(block);
+
+	try {
+		const responseData = await fetchData(queryIndexApiUrl);
+		console.log(responseData);
+		if (responseData) {
+			// Extract and render distinct categories and filters
+			const categories = getDistinctCategories(responseData);
+			renderFiltersAndCategoriesDropdown(block, categories, responseData);
+			// Render initial cards
+			renderCards(block, responseData);
+			// Handle searching functionality
+			let searchInput = block.querySelector('#search-input');
+			handleSearching(block, searchInput, responseData);
+
+			renderPagination(block, responseData);
+		} else {
+			console.error('No data fetched from API.');
+		}
+	} catch (error) {
+		console.error('Error fetching data:', error);
 	}
 }
 
+// Fetch data from API
 async function fetchData(apiUrl) {
 	try {
 		const response = await fetch(apiUrl);
@@ -34,10 +49,11 @@ async function fetchData(apiUrl) {
 		return data.data;
 	} catch (error) {
 		console.error("Error fetching data:", error);
-		return null;
+		return [];
 	}
 }
 
+// Render the search section with search input and filter/category dropdowns
 function renderSearchSection(block) {
 	// Create search filter container
 	const searchFilterContainer = document.createElement('div');
@@ -58,7 +74,6 @@ function renderSearchSection(block) {
 	searchContainer.appendChild(searchInput);
 
 	searchFilterContainer.appendChild(searchContainer);
-	handleSearching(block, searchInput);
 
 	// Create first select dropdown for filter
 	const filterDropdown = document.createElement('select');
@@ -88,93 +103,112 @@ function renderSearchSection(block) {
 	block.appendChild(searchFilterContainer);
 }
 
-function handleSearching(block, searchInputField) {
-	// Event listner for input in search field
+// Handle search input functionality
+function handleSearching(block, searchInputField, responseData) {
+	// Event listener for input in search field
+	let noResultDiv = block.querySelector('#articles-not-found-container');
+	let articlesContainer = block.querySelector('#articles-container');
+	let filterDropdown = block.querySelector('#filter-dropdown');
+	let categoryDropdown = block.querySelector('#category-dropdown');
+
 	searchInputField.addEventListener('input', function (event) {
-		const inputValue = event.target.value.trim();
-		const filteredArticlesByUserInput = responseData.filter(article => article.description.toLowerCase().includes(inputValue.toLowerCase()));
-		renderCards(block, filteredArticlesByUserInput);
-		renderPagination(block, filteredArticlesByUserInput);
-	});
-}
+		const inputValue = event.target.value.trim().toLowerCase();
+		let filteredData = [];
+		if (inputValue.length > 0) {
+			const filteredArticlesByUserInput = filterArticlesByDescription(responseData, inputValue);
+			filteredData = filteredArticlesByUserInput;
 
-function renderFiltersInDropdown() {
-	const filters = getfiltersArrayFromString();
-	console.log(filters);
-	const filtersDropdown = document.getElementById('filter-dropdown');
-	filters.forEach(filter => {
-		const option = document.createElement('option');
-		option.value = filter;
-		option.text = filter
-			.replace(/-/g, ' ') // Replace hyphens with spaces
-			.replace(/(^\w|\s\w)/g, m => m.toUpperCase()); // capitalizes the first letter of every word in a string
-		filtersDropdown.appendChild(option);
-
-		// Event listener for filter dropdown change
-		filtersDropdown.addEventListener('change', (event) => {
-			const selectedFilter = event.target.value;
-			console.log(selectedFilter);
-			if (selectedFilter && selectedFilter !== '') {
-				const sortedArticles = sortArticles(selectedFilter);
-				console.log(sortArticles);
-				renderCards('', sortedArticles);
-				renderPagination('', sortedArticles);
+			if (filteredArticlesByUserInput.length === 0) {
+				noResultDiv.style.display = 'block';
+				articlesContainer.style.display = 'none';
 			} else {
-				renderCards('', responseData); // Render all articles if no filter is selected
+				noResultDiv.style.display = 'none';
+				articlesContainer.style.display = 'block';
 			}
-			
-		});
+		} else {
+			filteredData = getFilteredDataBasedOnDropdown(responseData, filterDropdown.value, categoryDropdown.value);
+			noResultDiv.style.display = 'none';
+			articlesContainer.style.display = 'block';
+		}
+		renderCards(block, filteredData);
+		renderPagination(block, filteredData);
 	});
 }
 
-function sortArticles(selectedFilter) {
+function getFilteredDataBasedOnDropdown(data, filterValue, categoryValue) {
+	let filteredData = data;
+	
+	switch (true) {
+		case (filterValue !== '' && categoryValue !== ''):
+			const filteredDataByCategory = filterArticlesByCategory(filteredData, categoryValue);
+			filteredData = sortArticles(filterValue, filteredDataByCategory);
+			break;
+		case (filterValue !== '' && categoryValue === ''):
+			filteredData = sortArticles(filterValue, filteredData);
+			break;
+		case (filterValue === '' && categoryValue !== ''):
+			filteredData = filterArticlesByCategory(filteredData, categoryValue);
+			break;
+		default:
+			break;
+	}
+	return filteredData;
+}
+
+// Render filters and categories dropdown
+function renderFiltersAndCategoriesDropdown(block, categories, responseData) {
+  const filters = getFiltersArrayFromString();
+  const filtersDropdown = block.querySelector('#filter-dropdown');
+  const categoriesDropdown = block.querySelector('#category-dropdown');
+
+  filters.forEach(filter => {
+    const option = document.createElement('option');
+    option.value = filter;
+    option.text = formatFilterText(filter);
+    filtersDropdown.appendChild(option);
+  });
+
+  categories.forEach(category => {
+    const option = document.createElement('option');
+    option.value = category;
+    option.text = formatCategoryText(category);
+    categoriesDropdown.appendChild(option);
+  });
+
+  filtersDropdown.addEventListener('change', () => handleDropdownChange(block, filtersDropdown, categoriesDropdown, responseData));
+  categoriesDropdown.addEventListener('change', () => handleDropdownChange(block, filtersDropdown, categoriesDropdown, responseData));
+}
+
+// Handle dropdown change
+function handleDropdownChange(block, filtersDropdown, categoriesDropdown, responseData) {
+	console.log(responseData);
+  const selectedFilter = filtersDropdown.value;
+  const selectedCategory = categoriesDropdown.value;
+
+  const filteredAndSortedData = getFilteredDataBasedOnDropdown(responseData, selectedFilter, selectedCategory);
+	renderCards(block, filteredAndSortedData);
+	renderPagination(block, filteredAndSortedData);
+}
+
+function sortArticles(selectedFilter, data) {
 	if (selectedFilter === 'oldest-to-latest') {
-		return sortedResData.sort((a, b) => new Date(a.articlepublishdate) - new Date(b.articlepublishdate));
+		return data.sort((a, b) => new Date(a.articlepublishdate) - new Date(b.articlepublishdate));
 	} else if (selectedFilter === 'latest-to-oldest') {
-		return sortedResData.sort((a, b) => new Date(b.articlepublishdate) - new Date(a.articlepublishdate));
+		return data.sort((a, b) => new Date(b.articlepublishdate) - new Date(a.articlepublishdate));
 	}
 }
 
-function renderCategoriesInDropdown(categories) {
-	const categoriesDropdown = document.getElementById('category-dropdown');
-	categories.forEach(category => {
-		const option = document.createElement('option');
-		option.value = category;
-		option.text = category.replace(/(^\w|\s\w)/g, m => m.toUpperCase()); // capitalizes the first letter of every word in a string
-		categoriesDropdown.appendChild(option);
-	});
-
-	// Event listener for category dropdown change
-	categoriesDropdown.addEventListener('change', (event) => {
-		const selectedCategory = event.target.value;
-		console.log(selectedCategory);
-		if (selectedCategory && selectedCategory !== '') {
-			const filteredArticles = filterArticlesByCategory(responseData, selectedCategory);
-			sortedResData = [...filteredArticles];
-			console.log(filteredArticles);
-			renderCards('', filteredArticles);
-			renderPagination('', filterArticlesByCategory);
-		} else {
-			renderCards('', responseData); // Render all articles if no category is selected.
-			renderPagination('', responseData);
-		}
-	});
-}
-
-function filterArticlesByCategory(data, selectedCategory) {
-	// Filtered array to store matching articles
-	const filteredArticles = data.filter(article => article.category.toLowerCase() === selectedCategory.toLowerCase());
-	return filteredArticles;
-}
-
+// Render article cards
 function renderCards(block, data) {
-	let articlesContainer = document.getElementById('articles-container');
-	if (!articlesContainer && block != '') {
+	let articlesContainer = block.querySelector('#articles-container');
+	if (!articlesContainer) {
 		articlesContainer = document.createElement('div');
 		articlesContainer.id = "articles-container";
 		block.appendChild(articlesContainer);
 	}
+
 	articlesContainer.innerHTML = '';
+
 	const start = (currentPage - 1) * articlesPerPage;
 	const end = start + articlesPerPage;
 	const paginatedArticles = data.slice(start, end);
@@ -198,9 +232,17 @@ function renderCards(block, data) {
 	});
 }
 
+function createNoResultDiv(block) {
+	const notFoundContainer = document.createElement('div');
+	notFoundContainer.id = "articles-not-found-container";
+	notFoundContainer.style.display = 'none';
+	notFoundContainer.innerHTML = 'Sorry, Couldn’t find what you’re looking for.';
+	block.appendChild(notFoundContainer);
+}
+
 function renderPagination(block, data) {
-	let paginationContainer = document.getElementById('pagination-container');
-	if (!paginationContainer && block != '') {
+	let paginationContainer = block.querySelector('#pagination-container');
+	if (!paginationContainer) {
 		paginationContainer = document.createElement('div');
 		paginationContainer.id = "pagination-container";
 		block.appendChild(paginationContainer);
@@ -208,7 +250,6 @@ function renderPagination(block, data) {
 	paginationContainer.innerHTML = '';
 
 	let totalPages = Math.ceil(data.length / articlesPerPage);
-	console.log(totalPages);
 
 	if (totalPages > 1) {
 		// Left arrow button
@@ -263,6 +304,30 @@ function getDistinctCategories(data) {
 	return Array.from(categoriesSet);
 }
 
+// Filter articles by description
+function filterArticlesByDescription(data, input) {
+	return data.filter(article => article.description.toLowerCase().includes(input.toLowerCase()));
+}
+
+// Filter articles by category
+function filterArticlesByCategory(data, selectedCategory) {
+	const filteredArticles = data.filter(article => article.category.toLowerCase() === selectedCategory.toLowerCase());
+	return filteredArticles;
+}
+
+// Format category text
+function formatCategoryText(category) {
+	return category.replace(/(^\w|\s\w)/g, m => m.toUpperCase()); // Capitalize the first letter of every word
+}
+
+// Format filter text
+function formatFilterText(filterText) {
+	return filterText
+		.replace(/-/g, ' ') // Replace hyphens with spaces
+		.replace(/(^\w|\s\w)/g, m => m.toUpperCase()); // Capitalize the first letter of every word
+}
+
+// Format date
 function formatDate(serial) {
 	const date = excelDateToJSDate(serial);
 	const day = String(date.getDate()).padStart(2, '0'); // Ensure two digits for the day
@@ -271,13 +336,15 @@ function formatDate(serial) {
 	return `${day} ${month} ${year}`;
 }
 
+// Convert Excel date to JS date
 function excelDateToJSDate(serial) {
 	const excelEpoch = new Date(1900, 0, 1);
 	const date = new Date(excelEpoch.getTime() + (serial - 2) * 24 * 60 * 60 * 1000);
 	return date;
 }
 
-function getfiltersArrayFromString() {
+// Get filters array from string
+function getFiltersArrayFromString() {
 	if (!sortingFilters || sortingFilters.trim().length === 0) {
 		return [];
 	}
@@ -286,6 +353,7 @@ function getfiltersArrayFromString() {
 		: [sortingFilters];
 }
 
+// Get data attribute value by name
 function getDataAttributeValueByName(name) {
 	const element = document.querySelector(`[data-${name}]`);
 	return element ? element.getAttribute(`data-${name}`) : '';
