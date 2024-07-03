716c745cb6457cf58703354da8af89bac9c446c4
Sahil Kumar <sahilkumaryogesh@gmail.com>
2024-07-03 18:50:29 +0530

    ghfl-support-page

--- blocks/supportcards/supportcards.js
+++ blocks/supportcards/supportcards.js
@@ -1,9 +1,11 @@
 export default async function decorate(block) {
-// console.log('js called');
 
-getIndexedChildNames('support-landing-cards');
+  console.log(block);
 
-
+  // add all child div class name
+  getChildNames('support-landing-cards');
+  
+  // Add text in nackground image
   let element = document.querySelectorAll('.support-landing-cards-child')[1];
   let pictureElement = element.querySelector('picture');
   let img = pictureElement.querySelector('img').src;
@@ -20,6 +22,7 @@ getIndexedChildNames('support-landing-cards');
   ImgEle.parentElement.style.overflow = 'hidden';
   element.removeChild(element.firstElementChild);
 
+  // get all the child elements divs
   let zerothDiv = document.querySelectorAll('.support-landing-cards-child')[0];
   let firstDiv = document.querySelectorAll('.support-landing-cards-child')[1];
   let secondDiv = document.querySelectorAll('.support-landing-cards-child')[2];
@@ -29,76 +32,64 @@ getIndexedChildNames('support-landing-cards');
   let sixthDiv = document.querySelectorAll('.support-landing-cards-child')[6];
   let seventhDiv = document.querySelectorAll('.support-landing-cards-child')[7];
   let eigthDiv = document.querySelectorAll('.support-landing-cards-child')[8];
+  let parentEle= document.getElementsByClassName('support-landing-cards')[0];
+
+// get first partition div which contain customerquery, image, whats-up div
 const containerDivForCustomer = createDivElement('containerDivForCustomer');
-
-// image div
 const imageDiv = createDivElement('imageDiv');
-
-// get starting first div
 let forCustomerDivEle = zerothDiv;
 let imageDivEle = firstDiv;
 imageDiv.appendChild(forCustomerDivEle);
 imageDiv.appendChild(imageDivEle);
 containerDivForCustomer.appendChild(imageDiv);
-
-let parentEle= document.getElementsByClassName('support-landing-cards')[0];
-
-
 let whatupDivEle = fourthDiv;
 containerDivForCustomer.appendChild(whatupDivEle);
 
-// get second div
+// get second partition div which contain investor query, faq, gia chatbot
 const containerDivForInvestor = createDivElement('containerDivForInvestor');
 
+// append investor query and faq
 const faqDiv = createDivElement('faqDiv');
-
- // get starting first div
 let forInvestQuery = secondDiv;
-let forFaqElem =thirdDiv;
+let forFaqElem = thirdDiv;
 faqDiv.appendChild(forInvestQuery);
 faqDiv.appendChild(forFaqElem);
 containerDivForInvestor.appendChild(faqDiv);
 
-
+// append gia chatbot , info policy in one div and customer portal and branch locator in one div
 let newFifth=fifthDiv.querySelectorAll('div')[0];
 let newSixth= sixthDiv.querySelectorAll('div')[0];
-let newDiv=document.createElement('div');
-newDiv.appendChild(newFifth);
-newDiv.appendChild(newSixth);
-newDiv.classList.add("newDiv");
-fifthDiv.appendChild(newDiv);
+let GiaInfoDiv=createDivElement("gia-info-policy");
+GiaInfoDiv.appendChild(newFifth);
+GiaInfoDiv.appendChild(newSixth);
+fifthDiv.appendChild(GiaInfoDiv);
 
-let newSeventh=seventhDiv.querySelectorAll('div')[0];
+let newSeventh = seventhDiv.querySelectorAll('div')[0];
 let newEigth= eigthDiv.querySelectorAll('div')[0];
-let customDiv=document.createElement('div');
-customDiv.classList.add("customDiv");
-customDiv.appendChild(newSeventh);
-customDiv.appendChild(newEigth);
-fifthDiv.appendChild(customDiv);
-fifthDiv.classList.add("fifthElement");
+let customerPortalBranchLocDiv = createDivElement("customerPortal-branchLocator");
+customerPortalBranchLocDiv.appendChild(newSeventh);
+customerPortalBranchLocDiv.appendChild(newEigth);
+fifthDiv.appendChild(customerPortalBranchLocDiv);
 sixthDiv.remove('support-landing-cards-child');
 seventhDiv.remove('support-landing-cards-child');
 eigthDiv.remove('support-landing-cards-child');
 
 containerDivForInvestor.append(fifthDiv);
 
+// Add both div's in one container parentEle
 parentEle.appendChild(containerDivForCustomer);
 parentEle.appendChild(containerDivForInvestor);
-
 
 }
 
 // Function to get indexed child names and set class with numbering
-function getIndexedChildNames(parentId) {
+function getChildNames(parentId) {
   const newParentId = `.${parentId}`;
   const parentElement = document.querySelector(newParentId);
- 
   if (!parentElement) {
     return;
   }
- 
-  const childDivs = parentElement.querySelectorAll(`${newParentId} > div`);
- 
+  const childDivs = parentElement.querySelectorAll(`${newParentId} > div`); 
   childDivs.forEach((div, index) => {
     div.className = `${parentId}-child`;
   });
