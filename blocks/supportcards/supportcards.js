c80fe4313d49e8630307f52612cebace358bdbc7
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-04 11:26:10 +0530

    support page for both view

--- blocks/supportcards/supportcards.js
+++ blocks/supportcards/supportcards.js
@@ -1,13 +1,12 @@
 export default async function decorate(block) {
-
   // add all child div class name
   getChildNames('support-landing-cards');
 
   // Add text in nackground image
-  let element = document.querySelectorAll('.support-landing-cards-child')[1];
-  let pictureElement = element.querySelector('picture');
-  let img = pictureElement.querySelector('img').src;
-  let ImgEle = element.querySelectorAll('div')[1];
+  const element = document.querySelectorAll('.support-landing-cards-child')[1];
+  const pictureElement = element.querySelector('picture');
+  const img = pictureElement.querySelector('img').src;
+  const ImgEle = element.querySelectorAll('div')[1];
   ImgEle.style.backgroundImage = `url(${img})`;
   ImgEle.style.width = '317px';
   ImgEle.style.height = '281px';
@@ -21,26 +20,26 @@ export default async function decorate(block) {
   element.removeChild(element.firstElementChild);
 
   // get all the child elements divs
-  let zerothDiv = document.querySelectorAll('.support-landing-cards-child')[0];
-  let firstDiv = document.querySelectorAll('.support-landing-cards-child')[1];
-  let secondDiv = document.querySelectorAll('.support-landing-cards-child')[2];
-  let thirdDiv = document.querySelectorAll('.support-landing-cards-child')[3];
-  let fourthDiv = document.querySelectorAll('.support-landing-cards-child')[4];
-  let fifthDiv = document.querySelectorAll('.support-landing-cards-child')[5];
-  let sixthDiv = document.querySelectorAll('.support-landing-cards-child')[6];
-  let seventhDiv = document.querySelectorAll('.support-landing-cards-child')[7];
-  let eigthDiv = document.querySelectorAll('.support-landing-cards-child')[8];
-  let parentEle = document.getElementsByClassName('support-landing-cards')[0];
+  const zerothDiv = document.querySelectorAll('.support-landing-cards-child')[0];
+  const firstDiv = document.querySelectorAll('.support-landing-cards-child')[1];
+  const secondDiv = document.querySelectorAll('.support-landing-cards-child')[2];
+  const thirdDiv = document.querySelectorAll('.support-landing-cards-child')[3];
+  const fourthDiv = document.querySelectorAll('.support-landing-cards-child')[4];
+  const fifthDiv = document.querySelectorAll('.support-landing-cards-child')[5];
+  const sixthDiv = document.querySelectorAll('.support-landing-cards-child')[6];
+  const seventhDiv = document.querySelectorAll('.support-landing-cards-child')[7];
+  const eigthDiv = document.querySelectorAll('.support-landing-cards-child')[8];
+  const parentEle = document.getElementsByClassName('support-landing-cards')[0];
 
   // get first partition div which contain customerquery, image, whats-up div
   const containerDivForCustomer = createDivElement('containerDivForCustomer');
   const imageDiv = createDivElement('imageDiv');
-  let forCustomerDivEle = zerothDiv;
-  let imageDivEle = firstDiv;
+  const forCustomerDivEle = zerothDiv;
+  const imageDivEle = firstDiv;
   imageDiv.appendChild(forCustomerDivEle);
   imageDiv.appendChild(imageDivEle);
   containerDivForCustomer.appendChild(imageDiv);
-  let whatupDivEle = fourthDiv;
+  const whatupDivEle = fourthDiv;
   containerDivForCustomer.appendChild(whatupDivEle);
 
   // get second partition div which contain investor query, faq, gia chatbot
@@ -48,23 +47,23 @@ export default async function decorate(block) {
 
   // append investor query and faq
   const faqDiv = createDivElement('faqDiv');
-  let forInvestQuery = secondDiv;
-  let forFaqElem = thirdDiv;
+  const forInvestQuery = secondDiv;
+  const forFaqElem = thirdDiv;
   faqDiv.appendChild(forInvestQuery);
   faqDiv.appendChild(forFaqElem);
   containerDivForInvestor.appendChild(faqDiv);
 
   // append gia chatbot , info policy in one div and customer portal and branch locator in one div
-  let newFifth = fifthDiv.querySelectorAll('div')[0];
-  let newSixth = sixthDiv.querySelectorAll('div')[0];
-  let GiaInfoDiv = createDivElement("gia-info-policy");
+  const newFifth = fifthDiv.querySelectorAll('div')[0];
+  const newSixth = sixthDiv.querySelectorAll('div')[0];
+  const GiaInfoDiv = createDivElement('gia-info-policy');
   GiaInfoDiv.appendChild(newFifth);
   GiaInfoDiv.appendChild(newSixth);
   fifthDiv.appendChild(GiaInfoDiv);
 
-  let newSeventh = seventhDiv.querySelectorAll('div')[0];
-  let newEigth = eigthDiv.querySelectorAll('div')[0];
-  let customerPortalBranchLocDiv = createDivElement("customerPortal-branchLocator");
+  const newSeventh = seventhDiv.querySelectorAll('div')[0];
+  const newEigth = eigthDiv.querySelectorAll('div')[0];
+  const customerPortalBranchLocDiv = createDivElement('customerPortal-branchLocator');
   customerPortalBranchLocDiv.appendChild(newSeventh);
   customerPortalBranchLocDiv.appendChild(newEigth);
   fifthDiv.appendChild(customerPortalBranchLocDiv);
@@ -79,12 +78,12 @@ export default async function decorate(block) {
   parentEle.appendChild(containerDivForInvestor);
 
   // for mobile view
-  let accordionWrappers = document.getElementsByClassName('accordion-wrapper');
+  const accordionWrappers = document.getElementsByClassName('accordion-wrapper');
 
-  let titleContentEle = document.getElementsByClassName('mobilegrievancetab-wrapper');
+  const titleContentEle = document.getElementsByClassName('mobilegrievancetab-wrapper');
 
   if (titleContentEle.length > 0) {
-    const supportContainerDiv = document.createElement("div");
+    const supportContainerDiv = document.createElement('div');
     supportContainerDiv.className = 'table-content-container';
     supportContainerDiv.appendChild(titleContentEle[0]);
     const parentElement = document.querySelector('.accordion.support-table-name.block > div');
@@ -98,7 +97,7 @@ export default async function decorate(block) {
     console.error('Element with class "tabs-wrapper" not found.');
   }
 
-  Array.from(accordionWrappers).forEach(wrapper => {
+  Array.from(accordionWrappers).forEach((wrapper) => {
     const accordions = wrapper.querySelectorAll('.accordion > div');
 
     accordions.forEach((accordion) => {
@@ -114,7 +113,6 @@ export default async function decorate(block) {
       });
     });
   });
-
 }
 
 // change screen ui accordingly desktop and mobile view
@@ -151,16 +149,14 @@ function getChildNames(parentId) {
     return;
   }
   const childDivs = parentElement.querySelectorAll(`${newParentId} > div`);
-  childDivs.forEach((div, index) => {
+  childDivs.forEach((div) => {
     div.className = `${parentId}-child`;
   });
 }
 
 // create div's cntainer element
 function createDivElement(className) {
-  let divEle = document.createElement('div');
+  const divEle = document.createElement('div');
   divEle.className = className;
   return divEle;
 }
-
-
