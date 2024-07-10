1a85385874614574a0671db0ff259a0d82a07187
ImjeekxGurjar <jitender.rawat@techx-consulting.com>
2024-07-10 11:22:11 +0530

    fix apr calc

--- blocks/aprcalculator/aprcalculator.js
+++ blocks/aprcalculator/aprcalculator.js
@@ -154,20 +154,26 @@ function renderHTML(attributes) {
 
 // Function to update range input colors
 function updateRangeColors(block) {
-    const rangeInputs = block.querySelectorAll('input[type=range]');
+    const isMobileView = window.matchMedia("(max-width: 767px)").matches;
+    const mobileColor = '#f4f4f4';  //  color for mobile view
+    const desktopColor = '#fff'; // White color for desktop view
+ 
+    const rangeInputs = document.querySelectorAll('input[type=range]');
     rangeInputs.forEach(input => {
         const min = parseFloat(input.min);
         const max = parseFloat(input.max);
         const val = parseFloat(input.value);
         const normalizedValue = (val - min) / (max - min) * 100;
-        input.style.background = `linear-gradient(to right, #8CB133 ${normalizedValue}%, #fff ${normalizedValue}%)`;
+        const endColor = isMobileView ? mobileColor : desktopColor;
+        input.style.background = `linear-gradient(to right, #8CB133 ${normalizedValue}%, ${endColor} ${normalizedValue}%)`;
     });
 }
+ 
 
 // Function to update APR calculations and display
 function updateAPR(block) {
     const loanAmount = parseFloat(block.querySelector('#loanAmountAprRange').value);
-    const interestRate = parseFloat(block.querySelector('#interestRateAprRange').value).toFixed(1);
+    const interestRate = parseFloat(parseFloat(block.querySelector('#interestRateAprRange').value).toFixed(1));
     const loanTenureYears = parseFloat(block.querySelector('#loanTenureYearsAprRange').value);
     const loanTenureMonths = parseFloat(block.querySelector('#loanTenureMonthsAprRange').value);
     const originationCharges = parseFloat(block.querySelector('#loanOriginationChargesAprRange').value);
@@ -254,5 +260,7 @@ export default async function decorate(block) {
     updateAPR(block);
     addRangeInputListeners(block);
     addTextInputListeners(block);
-    setApplyNowButton(block,attributes)
+    setApplyNowButton(block,attributes);
 }
+window.addEventListener('resize',updateRangeColors);
+window.addEventListener('load',updateRangeColors);
