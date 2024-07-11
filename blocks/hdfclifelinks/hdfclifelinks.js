aad04324662f47d330ec2c3e159173cc12d74eea
Ankit saha <sahankit.0722@gmail.com>
2024-07-11 16:56:41 +0530

    third party product

--- blocks/hdfclifelinks/hdfclifelinks.js
+++ blocks/hdfclifelinks/hdfclifelinks.js
@@ -2,6 +2,17 @@ export default async function decorate(block){
     const childDivs = block.querySelectorAll(':scope > div');
     childDivs.forEach((div) => {
         div.classList.add('hdfc-life-child-div');
+        const link = div.querySelector('a.button');
+        console.log(link);
+        if(link){
+            const linkDiv = document.createElement('a');
+            linkDiv.href = link.href;
+            linkDiv.title = link.title;
+            linkDiv.appendChild(div.cloneNode(true));
+            div.replaceWith(linkDiv);
+            console.log(linkDiv);
+        }
     });
+    
 
 }
