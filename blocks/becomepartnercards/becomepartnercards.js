f09c2b811cda83888483fe3580d0d1199066ffc7
Ankit <ankit.saini@TechxSolution.com>
2024-07-24 17:53:04 +0530

    become a partner fixes

--- blocks/becomepartnercards/becomepartnercards.js
+++ blocks/becomepartnercards/becomepartnercards.js
@@ -4,20 +4,16 @@ export default async function decorate(block) {
     console.error('Main container not found');
     return;
   }
-  
   // Select all direct children divs of the container
   const parentDivs = block.querySelectorAll(':scope > div');
   parentDivs.forEach(parentDiv => {
     // Add the contentandpictureparent class to the parent div
     parentDiv.classList.add('contentandpictureparent');
-    
     // Select the first div (content) and the second div (picture) within each direct child div
     const firstDiv = parentDiv.querySelector('div:first-of-type');
     const secondDiv = parentDiv.querySelector('div:last-of-type');
-    
     if (firstDiv) {
-      firstDiv.classList.add('contentdetails');
-      
+      firstDiv.classList.add('card-contentdetails');
       // Convert <p> elements to <div> elements within the contentDiv
       const paragraphs = firstDiv.querySelectorAll('p');
       paragraphs.forEach(p => {
@@ -25,28 +21,20 @@ export default async function decorate(block) {
         div.innerHTML = p.innerHTML;
         p.replaceWith(div);
       });
-
       // Add specific classes to title and description
-      const title = firstDiv.querySelector('div:first-of-type');
-      const description = firstDiv.querySelector('div:last-of-type');
-      
-      if (title) {
-        title.classList.add('title');
-
+      const description = firstDiv.querySelector('div:first-of-type');
+      if (description) {
+        description.classList.add('card-description');
         // Split title text and wrap parts in spans
-        const titleText = title.textContent.trim();
-        const [firstWord, ...rest] = titleText.split(' ');
+        const descriptionText = description.textContent.trim();
+        const [firstWord, ...rest] = descriptionText.split(' ');
         const restOfTitle = rest.join(' ');
-
-        title.innerHTML = `
-          <span class="amount">${firstWord}</span>
-          ${restOfTitle ? `<span class="unit">${restOfTitle}</span>` : ''}
-        `;
+        description.innerHTML = `
+            <span class="amount">${firstWord}</span>
+            ${restOfTitle ? `<span class="unit">${restOfTitle}</span>` : ''}
+          `;
       }
-      
-      if (description) description.classList.add('description');
     }
-    
     if (secondDiv) {
       secondDiv.classList.add('picturedetails');
     }
