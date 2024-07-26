0f3aff69f2182c31ab5246ce00fc5c417492ad38
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-26 13:48:59 +0530

    teasercardsheadlines.js lint error resolve

--- blocks/teasercardsheadlines/teasercardsheadlines.js
+++ blocks/teasercardsheadlines/teasercardsheadlines.js
@@ -1,23 +1,23 @@
 
 function decorate(block) {
-  const pictureDivs = block.querySelectorAll(".teasercardsheadlines.block > div:first-child > div");
+  const pictureDivs = block.querySelectorAll('.teasercardsheadlines.block > div:first-child > div');
 
   if (pictureDivs.length > 0) {
     if (pictureDivs.length >= 1) {
-      pictureDivs[0].classList.add("desktop-picture");
+      pictureDivs[0].classList.add('desktop-picture');
     }
     if (pictureDivs.length >= 2) {
-      pictureDivs[1].classList.add("mobile-picture");
+      pictureDivs[1].classList.add('mobile-picture');
     }
   }
 
   const teaserContentDiv = block.querySelector('.teasercardsheadlines.block > div:last-child > div');
 
   if (teaserContentDiv) {
-    teaserContentDiv.classList.add("teaser-content");
+    teaserContentDiv.classList.add('teaser-content');
   }
 }
 
-const blockElement = document.querySelector(".teasercardsheadlines.block");
+const blockElement = document.querySelector('.teasercardsheadlines.block');
 
 decorate(blockElement);
