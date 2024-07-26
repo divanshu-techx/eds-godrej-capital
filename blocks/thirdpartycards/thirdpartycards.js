0d165388326ba7f79d60e10760b25042ae77ec2c
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-26 13:31:13 +0530

    thirdpartycards.js lint error resolve

--- blocks/thirdpartycards/thirdpartycards.js
+++ blocks/thirdpartycards/thirdpartycards.js
@@ -24,7 +24,6 @@ export default async function decorate(block) {
     });
   });
 
-
   // Adding a container class to manage the flex layout
   block.classList.add('third-party-cards-container');
 }
