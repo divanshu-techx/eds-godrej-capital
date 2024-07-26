c0b7f15a3a58a290619a40acff7fc61a2e58c736
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-26 13:44:23 +0530

    thankyoupopupfeedback.js lint error resolve

--- blocks/thankyoupopupfeedback/thankyoupopupfeedback.js
+++ blocks/thankyoupopupfeedback/thankyoupopupfeedback.js
@@ -1,25 +1,23 @@
 export default async function decorate(block) {
-
   const divElements = block.querySelectorAll('.thankyoupopupfeedback > div > div');
-  console.log(divElements);
   divElements.forEach((div, index) => {
     div.classList.add(`element-${index + 1}`);
   });
   // Create the close button element
-  var closeButton = document.createElement('button');
+  const closeButton = document.createElement('button');
   closeButton.className = 'close-button';
 
   // Add click event to close the popup
   closeButton.addEventListener('click', function () {
     document.querySelector('.feedback-title-btn').style = 'display:block';
-    var popup = block.parentElement;
+    const popup = block.parentElement;
     if (popup) {
       popup.style.display = 'none';
     }
   });
 
   // Append the close button to the popup
-  var popup = block;
+  const popup = block;
   if (popup) {
     popup.appendChild(closeButton);
   }
