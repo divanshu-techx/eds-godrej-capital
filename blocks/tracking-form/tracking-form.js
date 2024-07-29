eb1eff9ebffdfea68ba6aedd8ec7c34077e3dfcb
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-29 18:15:15 +0530

    usptabsblock.js code changes lint error resolve

--- blocks/tracking-form/tracking-form.js
+++ blocks/tracking-form/tracking-form.js
@@ -3,6 +3,62 @@ import { validateMobileNumber, validateEmail, restrictPhoneNumberInputs } from '
 
 // const formSheetUrl = getDataAttributeValueByName('sheeturl');
 
+function addCheckedClass(radio) {
+  block.querySelectorAll('.radio-wrapper label').forEach((label) => label.classList.remove('checked'));
+  const label = block.querySelector(`label[for="${radio.id}"]`);
+  if (label) {
+    label.classList.add('checked');
+  }
+}
+
+function toggleResidentDivVisibility(indianResidentRadio, indianMobileNumberDiv, nriMobileNumberDiv,
+  nonResidentIndianRadio) {
+  if (indianResidentRadio.checked) {
+    indianMobileNumberDiv.parentNode.style.display = 'block';
+    nriMobileNumberDiv.parentNode.style.display = 'none';
+    addCheckedClass(indianResidentRadio);
+  } else if (nonResidentIndianRadio.checked) {
+    indianMobileNumberDiv.parentNode.style.display = 'none';
+    nriMobileNumberDiv.parentNode.style.display = 'block';
+    addCheckedClass(nonResidentIndianRadio);
+  }
+}
+
+function toggleOtpMsgVisibility(indianResidentRadio, nriOtpMessage,
+  indianOtpMessage, nonResidentIndianRadio) {
+  if (indianResidentRadio.checked) {
+    nriOtpMessage.parentNode.style.display = 'none';
+    indianOtpMessage.parentNode.style.display = 'block';
+  } else if (nonResidentIndianRadio.checked) {
+    indianOtpMessage.parentNode.style.display = 'none';
+    nriOtpMessage.parentNode.style.display = 'block';
+  }
+}
+
+function toggleFormVisibility(hideSelector, showSelector, block) {
+  const hideElements = block.querySelectorAll(hideSelector);
+  const showElements = block.querySelectorAll(showSelector);
+  hideElements.forEach((el) => el.style.display = 'none');
+  showElements.forEach((el) => el.style.display = 'block');
+}
+
+function handleApplyNowBtn(block, form) {
+  const residentMobileField = block.querySelector('#mobileNumber');
+  const nriMobileField = block.querySelector('#nriMobileNumber');
+  const applyNowBtn = block.querySelector('#apply-now-btn');
+  if (applyNowBtn) {
+    applyNowBtn.addEventListener('click', function () {
+      if (validateMobileNumber(residentMobileField, 'Please enter a valid mobile number.') ||
+        validateMobileNumber(nriMobileField, 'Please enter a valid mobile number.') ||
+        validateEmail(nriMobileField)) {
+        toggleFormVisibility('.form1', '.form2', block);
+        toggleOtpMsgVisibility();
+        console.log(generatePayload(form));
+      }
+    });
+  }
+}
+
 export default async function decorate(block) {
   const formLink = block.querySelector('a[href$=".json"]');
   if (!formLink) return;
@@ -15,7 +71,7 @@ export default async function decorate(block) {
   const indianOtpMessage = block.querySelector('#indianOtpMessage');
   const nriOtpMessage = block.querySelector('#nriOtpMessage');
   if (indianResidentRadio && nonResidentIndianRadio
-     && indianMobileNumberDiv && nriMobileNumberDiv && indianOtpMessage && nriOtpMessage) {
+    && indianMobileNumberDiv && nriMobileNumberDiv && indianOtpMessage && nriOtpMessage) {
     // Select the first radio button by default
     indianResidentRadio.checked = true;
     // Add checked class to the corresponding label
@@ -28,64 +84,11 @@ export default async function decorate(block) {
     nonResidentIndianRadio.addEventListener('change', toggleResidentDivVisibility);
     handleApplyNowBtn(block, form);
     // Function to show or hide divs based on resident type radio button selection
-    function toggleResidentDivVisibility() {
-      if (indianResidentRadio.checked) {
-        indianMobileNumberDiv.parentNode.style.display = 'block';
-        nriMobileNumberDiv.parentNode.style.display = 'none';
-        addCheckedClass(indianResidentRadio);
-      } else if (nonResidentIndianRadio.checked) {
-        indianMobileNumberDiv.parentNode.style.display = 'none';
-        nriMobileNumberDiv.parentNode.style.display = 'block';
-        addCheckedClass(nonResidentIndianRadio);
-      }
-    }
+    toggleResidentDivVisibility(indianResidentRadio, indianMobileNumberDiv, nriMobileNumberDiv,
+      nonResidentIndianRadio);
 
-    function toggleOtpMsgVisibility() {
-      if (indianResidentRadio.checked) {
-        nriOtpMessage.parentNode.style.display = 'none';
-        indianOtpMessage.parentNode.style.display = 'block';
-      } else if (nonResidentIndianRadio.checked) {
-        indianOtpMessage.parentNode.style.display = 'none';
-        nriOtpMessage.parentNode.style.display = 'block';
-      }
-    }
+    toggleOtpMsgVisibility(indianResidentRadio, nriOtpMessage, indianOtpMessage, nonResidentIndianRadio);
 
-    function handleApplyNowBtn(block, form) {
-      const residentMobileField = block.querySelector('#mobileNumber');
-      const nriMobileField = block.querySelector('#nriMobileNumber');
-      const applyNowBtn = block.querySelector('#apply-now-btn');
-      if (applyNowBtn) {
-        applyNowBtn.addEventListener('click', function () {
-          if (validateMobileNumber(residentMobileField, 'Please enter a valid mobile number.') ||
-            validateMobileNumber(nriMobileField, 'Please enter a valid mobile number.') ||
-            validateEmail(nriMobileField)) {
-            toggleFormVisibility('.form1', '.form2', block);
-            toggleOtpMsgVisibility();
-            console.log(generatePayload(form));
-          }
-        });
-      }
-    }
-
-    function addCheckedClass(radio) {
-      block.querySelectorAll('.radio-wrapper label').forEach((label) => label.classList.remove('checked'));
-      const label = block.querySelector(`label[for="${radio.id}"]`);
-      if (label) {
-        label.classList.add('checked');
-      }
-    }
+    handleApplyNowBtn(block, form);
   }
 }
-
-function toggleFormVisibility(hideSelector, showSelector, block) {
-  const hideElements = block.querySelectorAll(hideSelector);
-  const showElements = block.querySelectorAll(showSelector);
-  hideElements.forEach((el) => el.style.display = 'none');
-  showElements.forEach((el) => el.style.display = 'block');
-}
-
-// // Get data attribute value by name
-// function getDataAttributeValueByName(name) {
-//   const element = document.querySelector(`[data-${name}]`);
-//   return element ? element.getAttribute(`data-${name}`) : '';
-// }
