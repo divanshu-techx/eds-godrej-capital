f4a52c9f0cf94bb299cc92ee8f5262035fd96c91
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-29 16:06:39 +0530

    utils.js lint error resolve

--- blocks/utils/drop-down.js
+++ blocks/utils/drop-down.js
@@ -6,22 +6,22 @@ export function initializeCustomSelect(selector) {
     var options = selElmnt.options;
     var ll = options.length;
     /* Create a new DIV that will act as the selected item */
-    var selectedDiv = document.createElement("DIV");
-    selectedDiv.setAttribute("class", "select-selected");
+    var selectedDiv = document.createElement('DIV');
+    selectedDiv.setAttribute('class', 'select-selected');
     selectedDiv.innerHTML = options[selElmnt.selectedIndex].innerHTML;
     el.parentNode.appendChild(selectedDiv);
 
     /* Create a new DIV that will contain the option list */
-    var optionsDiv = document.createElement("DIV");
-    optionsDiv.setAttribute("class", "select-items select-hide");
+    var optionsDiv = document.createElement('DIV');
+    optionsDiv.setAttribute('class', 'select-items select-hide');
 
     /* For each option in the original select element, create a new DIV that will act as an option item */
     for (var j = 0; j < ll; j++) {
-      var optionDiv = document.createElement("DIV");
+      var optionDiv = document.createElement('DIV');
       optionDiv.innerHTML = options[j].innerHTML;
-      optionDiv.addEventListener("click", function (e) {
+      optionDiv.addEventListener('click', function (e) {
         /* When an item is clicked, update the original select box, and the selected item */
-        var s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        var s = this.parentNode.parentNode.getElementsByTagName('select')[0];
         var sl = s.length;
         var h = this.parentNode.previousSibling;
         for (var i = 0; i < sl; i++) {
@@ -29,12 +29,12 @@ export function initializeCustomSelect(selector) {
             s.selectedIndex = i;
             h.innerHTML = this.innerHTML;
             var selectedItems =
-              this.parentNode.getElementsByClassName("same-as-selected");
+              this.parentNode.getElementsByClassName('same-as-selected');
             var yl = selectedItems.length;
             for (var k = 0; k < yl; k++) {
-              selectedItems[k].removeAttribute("class");
+              selectedItems[k].removeAttribute('class');
             }
-            this.setAttribute("class", "same-as-selected");
+            this.setAttribute('class', 'same-as-selected');
             break;
           }
         }
@@ -45,19 +45,19 @@ export function initializeCustomSelect(selector) {
 
     el.parentNode.appendChild(optionsDiv);
 
-    selectedDiv.addEventListener("click", function (e) {
+    selectedDiv.addEventListener('click', function (e) {
       /* When the select box is clicked, close any other select boxes, and open/close the current select box */
       e.stopPropagation();
       closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
+      this.nextSibling.classList.toggle('select-hide');
+      this.classList.toggle('select-arrow-active');
     });
   });
 
   function closeAllSelect(elmnt) {
     /* A function that will close all select boxes in the document, except the current select box */
-    var x = document.getElementsByClassName("select-items");
-    var y = document.getElementsByClassName("select-selected");
+    var x = document.getElementsByClassName('select-items');
+    var y = document.getElementsByClassName('select-selected');
     var xl = x.length;
     var yl = y.length;
     var arrNo = [];
@@ -65,16 +65,16 @@ export function initializeCustomSelect(selector) {
       if (elmnt == y[i]) {
         arrNo.push(i);
       } else {
-        y[i].classList.remove("select-arrow-active");
+        y[i].classList.remove('select-arrow-active');
       }
     }
     for (var i = 0; i < xl; i++) {
       if (arrNo.indexOf(i)) {
-        x[i].classList.add("select-hide");
+        x[i].classList.add('select-hide');
       }
     }
   }
 
   /* If the user clicks anywhere outside the select box, then close all select boxes */
-  document.addEventListener("click", closeAllSelect);
+  document.addEventListener('click', closeAllSelect);
 }
--- blocks/utils/google-map-styles.js
+++ blocks/utils/google-map-styles.js
@@ -2,137 +2,137 @@ const styles = [
   {
     elementType: 'geometry',
     stylers: [
-      { color: '#ebe3cd' }  // Light yellowish color for map background
+      { color: '#ebe3cd' },  // Light yellowish color for map background
     ]
   },
   {
     elementType: 'labels.icon',
     stylers: [
-      { visibility: 'off' }  // Hide all icons
+      { visibility: 'off' },  // Hide all icons
     ]
   },
   {
     elementType: 'labels.text.fill',
     stylers: [
-      { color: '#523735' }  // Dark brown text color
+      { color: '#523735' },  // Dark brown text color
     ]
   },
   {
     elementType: 'labels.text.stroke',
     stylers: [
-      { color: '#f5f1e6' }  // Light beige text stroke
+      { color: '#f5f1e6' },  // Light beige text stroke
     ]
   },
   {
     featureType: 'administrative',
     elementType: 'geometry.stroke',
     stylers: [
-      { color: '#c9b2a6' }  // Light brown borders for administrative areas
+      { color: '#c9b2a6' },  // Light brown borders for administrative areas
     ]
   },
   {
     featureType: 'administrative.country',
     elementType: 'labels.text.fill',
     stylers: [
-      { color: '#8b716a' }  // Light brown text color for country labels
+      { color: '#8b716a' },  // Light brown text color for country labels
     ]
   },
   {
     featureType: 'administrative.province',
     elementType: 'geometry.stroke',
     stylers: [
-      { color: '#9e8f7f' }  // Light grayish brown borders for provinces
+      { color: '#9e8f7f' },  // Light grayish brown borders for provinces
     ]
   },
   {
     featureType: 'landscape',
     elementType: 'geometry.fill',
     stylers: [
-      { color: '#e1d9c9' }  // Light beige color for landscape
+      { color: '#e1d9c9' },  // Light beige color for landscape
     ]
   },
   {
     featureType: 'poi',
     elementType: 'geometry',
     stylers: [
-      { color: '#dfd2ae' }  // Light yellowish color for Points of Interest
+      { color: '#dfd2ae' },  // Light yellowish color for Points of Interest
     ]
   },
   {
     featureType: 'poi',
     elementType: 'labels.text.fill',
     stylers: [
-      { color: '#93817c' }  // Dark grayish brown text color for POI labels
+      { color: '#93817c' },  // Dark grayish brown text color for POI labels
     ]
   },
   {
     featureType: 'poi.park',
     elementType: 'geometry.fill',
     stylers: [
-      { color: '#a5b076' }  // Light green color for parks
+      { color: '#a5b076' },  // Light green color for parks
     ]
   },
   {
     featureType: 'road',
     elementType: 'geometry.fill',
     stylers: [
-      { color: '#f5f1e6' }  // Light beige color for roads
+      { color: '#f5f1e6' },  // Light beige color for roads
     ]
   },
   {
     featureType: 'road',
     elementType: 'geometry.stroke',
     stylers: [
-      { color: '#e5d7c9' }  // Light brownish beige color for road borders
+      { color: '#e5d7c9' },  // Light brownish beige color for road borders
     ]
   },
   {
     featureType: 'road',
     elementType: 'labels.text.fill',
     stylers: [
-      { color: '#805b3e' }  // Dark brownish beige text color for road labels
+      { color: '#805b3e' },  // Dark brownish beige text color for road labels
     ]
   },
   {
     featureType: 'road.highway',
     elementType: 'geometry.fill',
     stylers: [
-      { color: '#f8c967' }  // Light orange color for highways
+      { color: '#f8c967' },  // Light orange color for highways
     ]
   },
   {
     featureType: 'road.highway',
     elementType: 'geometry.stroke',
     stylers: [
-      { color: '#e9bc62' }  // Light yellowish brown color for highway borders
+      { color: '#e9bc62' },  // Light yellowish brown color for highway borders
     ]
   },
   {
     featureType: 'transit',
     elementType: 'geometry.fill',
     stylers: [
-      { color: '#f3d3a7' }  // Light peach color for transit routes
+      { color: '#f3d3a7' },  // Light peach color for transit routes
     ]
   },
   {
     featureType: 'transit.station',
     elementType: 'labels.text.fill',
     stylers: [
-      { color: '#8a7c77' }  // Dark grayish brown text color for transit stations
+      { color: '#8a7c77' },  // Dark grayish brown text color for transit stations
     ]
   },
   {
     featureType: 'water',
     elementType: 'geometry.fill',
     stylers: [
-      { color: '#b9d3c2' }  // Light bluish green color for water bodies
+      { color: '#b9d3c2' },  // Light bluish green color for water bodies
     ]
   },
   {
     featureType: 'water',
     elementType: 'labels.text.fill',
     stylers: [
-      { color: '#92998d' }  // Dark grayish green text color for water labels
+      { color: '#92998d' }, // Dark grayish green text color for water labels
     ]
   }
 ];
