48f2092b82be4c9feed0f6331db4c84bf5b52991
Shrishti <shrishtikushwaha2498@gmail.com>
2024-07-31 15:52:06 +0530

    mediagallerycards.js.js code changes lint error resolve

--- blocks/mediagallerycards/mediagallerycards.js
+++ blocks/mediagallerycards/mediagallerycards.js
@@ -236,7 +236,8 @@ function createFilter(categories, block) {
 }
 
 function getYoutubeIdFromUrl(url) {
-  // const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  // const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)
+  // \/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
   const regex = /(?:youtube\.com\/(?:[^/\n\s]+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
   const match = url.match(regex);
   return match ? match[1] : null;
