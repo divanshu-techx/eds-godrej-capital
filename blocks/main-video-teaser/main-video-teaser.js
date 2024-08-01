e0aabd2c2ab239ab108ee8c9bcf44c4d49899e0a
Shrishti <shrishtikushwaha2498@gmail.com>
2024-08-01 12:36:50 +0530

    main-video-teaser.js code changes lint error resolve

--- blocks/main-video-teaser/main-video-teaser.js
+++ blocks/main-video-teaser/main-video-teaser.js
@@ -125,6 +125,100 @@ function createModal(videoUrl) {
   };
 }
 
+function initBackgroundPosition(classList, breakpoint) {
+  const classPrefixes = {
+    [MEDIA_BREAKPOINTS.MOBILE]: 's',
+    [MEDIA_BREAKPOINTS.TABLET]: 'm',
+    [MEDIA_BREAKPOINTS.DESKTOP]: 'l',
+  };
+  const classPrefix = classPrefixes[breakpoint];
+  const backgroudPositionClass = [...classList].find((item) => item.startsWith(`bp-${classPrefix}-`));
+
+  let backgroundPositionValue = 'unset';
+
+  if (backgroudPositionClass) {
+    let [, , xPosition, yPosition] = backgroudPositionClass.split('-');
+
+    // workaround, '-' character classes are not supported
+    // so for '-45px' we need to put 'm45px'
+    xPosition = xPosition.replace('m', '-');
+    yPosition = yPosition.replace('m', '-');
+
+    backgroundPositionValue = `${xPosition} ${yPosition}`;
+  }
+
+  return backgroundPositionValue;
+}
+
+function getImageForBreakpoint(imagesList, onChange = () => { }) {
+  const mobileMQ = window.matchMedia('(max-width: 743px)');
+  const tabletMQ = window.matchMedia(
+    '(min-width: 744px) and (max-width: 1199px)',
+  );
+  const desktopMQ = window.matchMedia('(min-width: 1200px)');
+
+  const [mobilePic, tabletPic, desktopPic] = imagesList.querySelectorAll('picture');
+
+  const onBreakpointChange = (mq, picture, breakpoint) => {
+    if (mq.matches) {
+      onChange(picture, breakpoint);
+    }
+  };
+  const onMobileChange = (mq) => onBreakpointChange(mq, mobilePic, MEDIA_BREAKPOINTS.MOBILE);
+  const onTabletChange = (mq) => onBreakpointChange(mq, tabletPic, MEDIA_BREAKPOINTS.TABLET);
+  const onDesktopChange = (mq) => onBreakpointChange(mq, desktopPic, MEDIA_BREAKPOINTS.DESKTOP);
+
+  mobileMQ.addEventListener('change', onMobileChange);
+  tabletMQ.addEventListener('change', onTabletChange);
+  desktopMQ.addEventListener('change', onDesktopChange);
+
+  if (mobileMQ.matches) {
+    onMobileChange(mobileMQ);
+    return;
+  }
+
+  if (tabletMQ.matches) {
+    onTabletChange(tabletMQ);
+    return;
+  }
+  onDesktopChange(desktopMQ);
+}
+
+function prepareBackgroundImage(block) {
+  const onBackgroundImgChange = (imgEl, backgroundTarget, breakpoint) => {
+    const backgroundPostionStyles = initBackgroundPosition(
+      block.classList,
+      breakpoint,
+    );
+    const backgroundSrc = imgEl.currentSrc;
+    backgroundTarget.style.backgroundImage = `url(${backgroundSrc})`;
+    backgroundTarget.style.backgroundPosition = backgroundPostionStyles;
+  };
+
+  const onBreakpointChange = (pictureEl, breakpoint) => {
+    const pictureClone = pictureEl.cloneNode(true);
+    const img = pictureClone.querySelector('img');
+    pictureClone.classList.add('v2-dlt__picture');
+
+    block.append(pictureClone);
+
+    if (img.currentSrc) {
+      onBackgroundImgChange(img, block, breakpoint);
+      pictureClone.remove();
+    } else {
+      img.addEventListener('load', () => {
+        onBackgroundImgChange(img, block, breakpoint);
+        pictureClone.remove();
+      });
+    }
+  };
+
+  const listOfPictures = block.querySelector('ul');
+  // removing from DOM - prevent loading all of provided images
+  listOfPictures.remove();
+  getImageForBreakpoint(listOfPictures, onBreakpointChange);
+}
+
 // Main Function
 export default async function decorate(block) {
   prepareBackgroundImage(block);
@@ -174,7 +268,7 @@ export default async function decorate(block) {
   const clickable = block.querySelectorAll('.button, a');
 
   clickable.forEach((el) => {
-    el.addEventListener('click', function (event) {
+    el.addEventListener('click', (event) => {
       event.preventDefault();
 
       const videoUrl = el.getAttribute('href');
@@ -263,98 +357,5 @@ export default async function decorate(block) {
   });
 }
 
-function getImageForBreakpoint(imagesList, onChange = () => { }) {
-  const mobileMQ = window.matchMedia('(max-width: 743px)');
-  const tabletMQ = window.matchMedia(
-    '(min-width: 744px) and (max-width: 1199px)',
-  );
-  const desktopMQ = window.matchMedia('(min-width: 1200px)');
 
-  const [mobilePic, tabletPic, desktopPic] = imagesList.querySelectorAll('picture');
 
-  const onBreakpointChange = (mq, picture, breakpoint) => {
-    if (mq.matches) {
-      onChange(picture, breakpoint);
-    }
-  };
-  const onMobileChange = (mq) => onBreakpointChange(mq, mobilePic, MEDIA_BREAKPOINTS.MOBILE);
-  const onTabletChange = (mq) => onBreakpointChange(mq, tabletPic, MEDIA_BREAKPOINTS.TABLET);
-  const onDesktopChange = (mq) => onBreakpointChange(mq, desktopPic, MEDIA_BREAKPOINTS.DESKTOP);
-
-  mobileMQ.addEventListener('change', onMobileChange);
-  tabletMQ.addEventListener('change', onTabletChange);
-  desktopMQ.addEventListener('change', onDesktopChange);
-
-  if (mobileMQ.matches) {
-    onMobileChange(mobileMQ);
-    return;
-  }
-
-  if (tabletMQ.matches) {
-    onTabletChange(tabletMQ);
-    return;
-  }
-  onDesktopChange(desktopMQ);
-}
-
-function prepareBackgroundImage(block) {
-  const onBackgroundImgChange = (imgEl, backgroundTarget, breakpoint) => {
-    const backgroundPostionStyles = initBackgroundPosition(
-      block.classList,
-      breakpoint,
-    );
-    const backgroundSrc = imgEl.currentSrc;
-    backgroundTarget.style.backgroundImage = `url(${backgroundSrc})`;
-    backgroundTarget.style.backgroundPosition = backgroundPostionStyles;
-  };
-
-  const onBreakpointChange = (pictureEl, breakpoint) => {
-    const pictureClone = pictureEl.cloneNode(true);
-    const img = pictureClone.querySelector('img');
-    pictureClone.classList.add('v2-dlt__picture');
-
-    block.append(pictureClone);
-
-    if (img.currentSrc) {
-      onBackgroundImgChange(img, block, breakpoint);
-      pictureClone.remove();
-    } else {
-      img.addEventListener('load', () => {
-        onBackgroundImgChange(img, block, breakpoint);
-        pictureClone.remove();
-      });
-    }
-  };
-
-  const listOfPictures = block.querySelector('ul');
-  // removing from DOM - prevent loading all of provided images
-  listOfPictures.remove();
-  getImageForBreakpoint(listOfPictures, onBreakpointChange);
-}
-
-function initBackgroundPosition(classList, breakpoint) {
-  const classPrefixes = {
-    [MEDIA_BREAKPOINTS.MOBILE]: 's',
-    [MEDIA_BREAKPOINTS.TABLET]: 'm',
-    [MEDIA_BREAKPOINTS.DESKTOP]: 'l',
-  };
-  const classPrefix = classPrefixes[breakpoint];
-  const backgroudPositionClass = [...classList].find((item) =>
-    item.startsWith(`bp-${classPrefix}-`),
-  );
-
-  let backgroundPositionValue = 'unset';
-
-  if (backgroudPositionClass) {
-    let [, , xPosition, yPosition] = backgroudPositionClass.split('-');
-
-    // workaround, '-' character classes are not supported
-    // so for '-45px' we need to put 'm45px'
-    xPosition = xPosition.replace('m', '-');
-    yPosition = yPosition.replace('m', '-');
-
-    backgroundPositionValue = `${xPosition} ${yPosition}`;
-  }
-
-  return backgroundPositionValue;
-}
