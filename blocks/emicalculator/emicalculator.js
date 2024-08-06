e595da0cd2941eab6055370fa6e03c5868d92474
manoj <manoj.kumar@techx-consulting.com>
2024-08-06 13:35:10 +0530

    fix the lint issue in emi calculator

--- blocks/emicalculator/emicalculator.js
+++ blocks/emicalculator/emicalculator.js
@@ -53,7 +53,7 @@ function createElement(type, attributes = {}, ...children) {
   Object.entries(attributes).forEach(([key, value]) => {
     element.setAttribute(key, value);
   });
-    
+  
   children.forEach((child) => {
     if (typeof child === 'string') {
       element.appendChild(document.createTextNode(child));
