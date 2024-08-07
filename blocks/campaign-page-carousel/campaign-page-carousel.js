7413a8ed8d8f684f70277b3976287bfb677c0ec9
ImjeekxGurjar <jitender.rawat@techx-consulting.com>
2024-08-07 16:50:20 +0530

    padding adjustment

--- blocks/campaign-page-carousel/campaign-page-carousel.js
+++ blocks/campaign-page-carousel/campaign-page-carousel.js
@@ -2,7 +2,7 @@ function startTimer(block) {
     return setInterval(() => {
       const rightSwip = block.querySelector('.swip-right');
       rightSwip.click();
-    }, 5000);
+    }, 500000);
   }
   
   let timer;
